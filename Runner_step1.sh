e26936e27d0944d79cd9731ac7229e0a04d75bb1
Mehdi Keshani <m.keshani@tudelft.nl>
2022-09-12 11:47:11 +0200

    update bash of the first phase of experiment to run wala

--- Runner_step1.sh
+++ Runner_step1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=0
-folder="$2-$3"
+folder="$2"
 mkdir $folder
 while IFS= read -r line; do
   if [ $i -ne 0 ]; then
@@ -8,12 +8,17 @@ while IFS= read -r line; do
     mkdir $folder/$coord
     mkdir $folder/$coord/opal
     mkdir $folder/$coord/merge
-    echo "Generating graphs for "$coord" package..."
-    java -Xmx7000m -Xms7000m -jar runnable/LightWeightCGs-1.0-SNAPSHOT-with-dependencies.jar --opal "$line" $folder/$coord/opal $3 > $folder/$coord/opal/log 2>&1
-    java -Xmx7000m -Xms7000m -jar runnable/LightWeightCGs-1.0-SNAPSHOT-with-dependencies.jar --merge "$line" $folder/$coord/merge $3 > $folder/$coord/merge/log 2>&1
+    mkdir $folder/$coord/wala
+    echo -n $i"- Generating graphs for "$coord" package: "
+    echo -n " opal ..."
+    java -Xmx7000m -Xms7000m -jar runnable/LightWeightCGs-1.0-SNAPSHOT-with-dependencies.jar --opal "$line" $folder/$coord/opal > $folder/$coord/opal/log 2>&1
+    echo -n ", wala ..."
+    java -Xmx7000m -Xms7000m -jar runnable/LightWeightCGs-1.0-SNAPSHOT-with-dependencies.jar --wala "$line"  $folder/$coord/wala > $folder/$coord/wala/log 2>&1
+    echo ", merge ..."
+    java -Xmx7000m -Xms7000m -jar runnable/LightWeightCGs-1.0-SNAPSHOT-with-dependencies.jar --merge "$line" $folder/$coord/merge > $folder/$coord/merge/log 2>&1
   fi
   ((i = i + 1))
 done <"$1"
 
 #How to run => $1: input file, $2: output folder $3: algorithm
-#bash Runner_step1.sh results/inputMvnData/highly.connected10.resolved.csv results/outputStats/cgs CHA
+#bash Runner_step1.sh results/inputMvnData/highly.connected10.resolved.csv results/outputStats/cgs
